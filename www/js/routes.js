95665b5937434a0ee887f9e4b3764e9622da05a3
hideki16 <gabriel_shippo@hotmail.com>
2016-04-27 20:49:53 -0300

    remove route

--- www/js/routes.js
+++ www/js/routes.js
@@ -6,11 +6,7 @@ angular.module('starter.routes', [])
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
-  .state('map', {
-     url: '/map',
-     templateUrl: 'templates/map.html',
-     controller: 'MapCtrl'
-   })
+
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
